8a5af5554d3780cbe8cf3920d73ebeffb9e63d5b
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2024-08-30 23:09:07 +0500

    Footer Added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+          Find, book, rent a car — quick and super easily!
         </h1>
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
